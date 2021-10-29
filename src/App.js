ef76e5eb40e213d10e4baddf22ecb320d30927b4
Hillol Deb <46314234+hilloldeb@users.noreply.github.com>
2021-10-29 17:44:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Routes from './routes';
+
+import GithubCorner from './components/GithubCorner';
+
+function App() {
+  return (
+    <div id="app">
+      <GithubCorner />
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
